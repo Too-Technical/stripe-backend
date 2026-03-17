ac2e0b42753bee5b17353fb59c18dbafc6e3e059
Too-Technical <164341301+Too-Technical@users.noreply.github.com>
2026-03-17 18:02:33 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const prices = await stripe.prices.list({
+      expand: ['data.product'],
+      active: true
+    });
+
+    const products = prices.data.map(p => ({
+      priceId: p.id,
+      name: p.product.name,
+      price: p.unit_amount,
+    }));
+
+    res.json(products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
+});
